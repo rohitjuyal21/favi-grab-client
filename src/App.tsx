f4b749c4133c03244ba2e183260ea0fa16b28116
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-08 02:04:29 +0530

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <main className="bg-background font-poppins">
-      <div className="max-w-screen-lg mx-auto p-8">
+    <main className="min-h-screen bg-background font-poppins flex">
+      <div className="flex flex-col max-w-screen-lg w-full mx-auto p-4 sm:p-8">
         <Header />
         <Main />
+        <Footer />
       </div>
       <Toaster />
     </main>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-sm text-muted-foreground text-center mt-auto">
+      © 2024 FaviGrab. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
