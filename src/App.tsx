a52102ddd6d9fc9459da42762e4a69fc5bc4ec74
rohitjuyal21 <rohitjuyal2003@gmail.com>
2024-09-13 00:05:02 +0530

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Footer />
       </div>
       <Toaster />
+      <Analytics />
     </main>
   );
 }
